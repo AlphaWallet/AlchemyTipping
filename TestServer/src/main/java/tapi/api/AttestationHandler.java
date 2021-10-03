ac1cc03653d949c01729611abfa4dab502aaf193
James Brown <soylentblue@hotmail.co.uk>
2021-10-04 10:29:54 +1100

    Remove all secrets from source, invalidate leaked keys and regenerate replacement keys. Use new ethereum private key for Attestation signing

--- TestServer/src/main/java/tapi/api/AttestationHandler.java
+++ TestServer/src/main/java/tapi/api/AttestationHandler.java
@@ -1,51 +1,40 @@
 package tapi.api;
 
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
-import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
-import org.bouncycastle.crypto.params.*;
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
+import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
+import org.bouncycastle.crypto.params.ECPublicKeyParameters;
 import org.bouncycastle.math.ec.ECMultiplier;
 import org.bouncycastle.math.ec.ECPoint;
 import org.bouncycastle.math.ec.FixedPointCombMultiplier;
 import org.web3j.crypto.ECKeyPair;
-import org.web3j.crypto.Sign;
 import org.web3j.utils.Numeric;
-import tapi.api.crypto.*;
+import tapi.api.crypto.IdentifierAttestation;
+import tapi.api.crypto.SignedIdentifierAttestation;
 import tapi.api.crypto.core.SignatureUtility;
 
 import java.io.IOException;
-import java.security.SecureRandom;
 
 import static tapi.api.crypto.core.SignatureUtility.ECDSA_DOMAIN;
 
 public class AttestationHandler {
     private static AsymmetricCipherKeyPair attestorKeys;
-    private static SecureRandom rand;
 
+    //Generate Attestation signing AsymmetricCipherKeyPair from Ethereum private key in the keys.secret file
     public static void setupKeys(String keyHex)
     {
         try {
-            rand = SecureRandom.getInstance("SHA1PRNG");
-            rand.setSeed("seed".getBytes());
-            AsymmetricCipherKeyPair dud = SignatureUtility.constructECKeys(rand);
-            //attestorKeys = SignatureUtility.constructECKeys(rand);
-
             ECKeyPair attestationKeyPair = ECKeyPair.create(Numeric.toBigInt(keyHex));
-
-            ECKeyPairGenerator generator = new ECKeyPairGenerator();
-            ECDomainParameters params = ECDSA_DOMAIN;
             ECPrivateKeyParameters privKey = new ECPrivateKeyParameters(attestationKeyPair.getPrivateKey(), ECDSA_DOMAIN);
 
-            ECPoint Q = createBasePointMultiplier().multiply(params.getG(), attestationKeyPair.getPrivateKey());
+            ECPoint Q = createBasePointMultiplier().multiply(ECDSA_DOMAIN.getG(), attestationKeyPair.getPrivateKey());
             ECPublicKeyParameters publicKey = new ECPublicKeyParameters(Q, ECDSA_DOMAIN);
-
-            //ECKeyGenerationParameters keygenParams = new ECKeyGenerationParameters(ECDSA_DOMAIN, random);
 
             attestorKeys = new AsymmetricCipherKeyPair(
                     publicKey,
                     privKey);
 
             System.out.println("Attestor Address: " + SignatureUtility.addressFromKey(attestorKeys.getPublic()));
-
         }
         catch (Exception e)
         {
